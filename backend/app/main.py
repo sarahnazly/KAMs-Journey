7b894fe2519b391211685458a1272d9c39d11186
anjanianadya <ratunadyaanjania@gmail.com>
2025-12-09 14:07:29 +0700

    feat(ep): include evaluation prediction router in main app

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routers.feature_importance import router as fi_router
 from app.routers.win_probability import router as wp_router
+from app.routers.evaluation_prediction_router import router as ep_router
 from app.routers.orientasi_router import router as ori_router
 from app.routers.pelaksanaan_router import router as pel_router
 from app.routers.kinerja_router import router as kin_router
@@ -25,6 +26,7 @@ app.add_middleware(
 
 app.include_router(fi_router)
 app.include_router(wp_router)
+app.include_router(ep_router)
 app.include_router(ori_router)
 app.include_router(pel_router)
 app.include_router(kin_router)

4456d5c2998896bde43cc45c687091fd4605d123
anjanianadya <ratunadyaanjania@gmail.com>
2025-12-09 14:16:07 +0700

    chore(fi): change R2 score to R² score

--- frontend/src/components/dashboard/FeatureImportance.tsx
+++ frontend/src/components/dashboard/FeatureImportance.tsx
@@ -226,7 +226,7 @@ export default function FeatureImportanceSection({
         message={
           <>
             This prediction uses <b>{model.name}</b> algorithm with{" "}
-            <b>{formatR2(model.R2)} R2 score</b>. The model was trained using{" "}
+            <b>{formatR2(model.R2)} R² score</b>. The model was trained using{" "}
             <b>{model.trainCount} data</b>.
           </>
         }
